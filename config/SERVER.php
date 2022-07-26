c138e0bc939dff6577bfddec233da3aef70c7398
RangelJhoan <jhoanrangels2@gmail.com>
2022-07-26 00:15:21 -0500

    feat: Se inicia la configuración del proyecto
    Se inicia la configuración del servidor apache y se definen las constantes necesarias para la configuración del sistema y la base de datos.
    
    --  htaccess  --
    * Se configura la estructura del proyecto para que no se visualice el contenido de los directorios.
    * Se ajusta regla para que tome Index como vista principal y que en la dirección URL sólo se permitan caracteres que posibiliten la navegación MVC.
    
    --  APP  --
    * Se agregan constantes para la configuración del sistema como la dirección del servidor, la zona horaria, entre otros.
    
    --  SERVER  --
    * Se agregan constantes para la configuración de la base de datos.
    * Se agregan las llaves secretas y el método de encriptación.
    
    NOTA:
    El proceso de configuración sigue en desarrollo.

---
+++ config/SERVER.php
@@ -0,0 +1,14 @@
+<?php
+
+const SERVER = null;
+const DB = null;
+const USER = null;
+const PASS = null;
+
+const SGBD = "mysql:host=".SERVER.";dbname=".DB;
+
+const ENCRYPTION_METHOD = null;
+const SECRET_KEY = null;
+const SECRET_IV = null;
+
+?>
