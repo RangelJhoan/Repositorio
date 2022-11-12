973644fa7b9cbeee1637f3716f56f23930b45596
RangelJhoan <jhoanrangels2@gmail.com>
2022-11-11 21:08:12 -0500

    AJUSTE - Se agrega validación para que no se repitan los programas
    Se agrega validación en el editar y registrar programas para que no se repitan los nombres dentro de la base de datos.

--- controladores/programaControlador.php
+++ controladores/programaControlador.php
@@ -29,26 +29,39 @@ class programaControlador extends programaModelo{
             exit();
         }
 
-        $agregar_programa = programaModelo::agregar_programa_modelo($programa);
+        $check_programa = mainModel::ejecutar_consulta_simple("SELECT id FROM programa WHERE nombre = '".$programa->getNombre()."';");
 
-        if($agregar_programa->rowCount() == 1){
-            $alerta=[
-                "Alerta"=>"recargar",
-                "Titulo"=>"Exitoso",
-                "Texto"=>"Programa creado correctamente",
-                "Tipo"=>"success"
-            ];
-            echo json_encode($alerta);
-            exit();
-        }else{
+        if($check_programa->rowCount() > 0){
             $alerta=[
                 "Alerta"=>"simple",
                 "Titulo"=>"Error",
-                "Texto"=>"Error al crear el programa",
+                "Texto"=>"El programa ya se encuentra registrado en el repositorio",
                 "Tipo"=>"error"
             ];
             echo json_encode($alerta);
             exit();
+        }else{
+            $agregar_programa = programaModelo::agregar_programa_modelo($programa);
+
+            if($agregar_programa->rowCount() == 1){
+                $alerta=[
+                    "Alerta"=>"recargar",
+                    "Titulo"=>"Exitoso",
+                    "Texto"=>"Programa creado correctamente",
+                    "Tipo"=>"success"
+                ];
+                echo json_encode($alerta);
+                exit();
+            }else{
+                $alerta=[
+                    "Alerta"=>"simple",
+                    "Titulo"=>"Error",
+                    "Texto"=>"Error al crear el programa",
+                    "Tipo"=>"error"
+                ];
+                echo json_encode($alerta);
+                exit();
+            }
         }
     }
 
@@ -118,24 +131,37 @@ class programaControlador extends programaModelo{
             exit();
         }
 
-        $editarPrograma = programaModelo::editar_programa_modelo($programa);
-        if($editarPrograma->rowCount() > 0){
-            $alerta=[
-                "Alerta"=>"redireccionar",
-                "Titulo"=>"Datos actualizados",
-                "URL"=>"http://localhost/Repositorio/adminProgramas/",
-                "Texto"=>"Los datos han sido actualizados con éxito",
-                "Tipo"=>"success"
-            ];
-        }else{
+        $check_programa = mainModel::ejecutar_consulta_simple("SELECT id FROM programa WHERE nombre = '".$programa->getNombre()."';");
+
+        if($check_programa->rowCount() > 0){
             $alerta=[
                 "Alerta"=>"simple",
                 "Titulo"=>"Error",
-                "Texto"=>"No se pudo actualizar la información",
+                "Texto"=>"El programa ya se encuentra registrado en el repositorio",
                 "Tipo"=>"error"
             ];
+            echo json_encode($alerta);
+            exit();
+        }else{
+            $editarPrograma = programaModelo::editar_programa_modelo($programa);
+            if($editarPrograma->rowCount() > 0){
+                $alerta=[
+                    "Alerta"=>"redireccionar",
+                    "Titulo"=>"Datos actualizados",
+                    "URL"=>"http://localhost/Repositorio/adminProgramas/",
+                    "Texto"=>"Los datos han sido actualizados con éxito",
+                    "Tipo"=>"success"
+                ];
+            }else{
+                $alerta=[
+                    "Alerta"=>"simple",
+                    "Titulo"=>"Error",
+                    "Texto"=>"No se pudo actualizar la información",
+                    "Tipo"=>"error"
+                ];
+            }
+            echo json_encode($alerta);
         }
-        echo json_encode($alerta);
     }
 
     /**
