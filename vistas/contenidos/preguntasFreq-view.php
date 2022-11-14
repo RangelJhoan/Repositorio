75397bd0de5072b8778f436ca31719a83b65b386
Camilo Valencia <61189092+ValenciaJCamilo@users.noreply.github.com>
2022-11-14 14:09:43 -0500

    Preguntas Frecuentes  finalizado
    1. Se ajusta el diseño css
    2. Se ajusta el contenido de la vista (preguntas y respuestas)
    
    IMPORTANTE: Toca hacer controlador para poder manipular esta vista y las demás del home...
    Al momento de hacer clic en "preguntas frecuentes" se debe redirigir al usuario a esta vista porque NO ESTÁ FUNCIONANDO

--- vistas/contenidos/preguntasFreq-view.php
+++ vistas/contenidos/preguntasFreq-view.php
@@ -19,15 +19,14 @@
                 </div>
             </label>
             <div class="contentFAQ">
-                <p class="answerFAQ">Un repositorio institucional es una fuente Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio maiores ullam ab laborum, deleniti magnam! Iure asperiores libero tempora pariatur rem quas, quaerat quod consequatur rerum incidunt sit, modi quae.
-                Molestiae, quis. Maxime dolores ratione, eaque iste numquam consequatur recusandae sed praesentium rerum in, laborum nam consequuntur illo ea, quaerat vitae placeat ducimus iusto asperiores? Culpa dolores odio cumque nostrum!</p>
+                <p class="answerFAQ">Un repositorio institucional es un espacio en donde se reúne, preserva y divulgan archivos en formato digital con información obtenida a través de desarrollos científicos o académicos de una institución. Se caracterizan porque las Instituciones de Educación Superior (IES) son las encargadas de generar y suministrar los contenidos que se encuentren disponibles.</p>
             </div>
         </div>
         <!-- Pregunta #2 -->
         <div class="parentTabFAQ">
             <input type="radio" name="tab" id="tab-2" class="inputFAQ">
             <label for="tab-2" class="labelFAQ">
-                <span class="questionFAQ">¿Para quienes va dirigido el repositorio institucional?</span>
+                <span class="questionFAQ">¿Para quiénes va dirigido el repositorio institucional?</span>
                 <div class="iconContainer">
                     <div class="iconFAQ">
                         <i class="uil uil-angle-down"></i>
@@ -35,14 +34,14 @@
                 </div>
             </label>
             <div class="contentFAQ">
-                <p class="answerFAQ">El repositorio va dirigido principalemte a los estudiantes de ingeniería de sistemas, sin embargo, existe la posibilidad de poder publicar y encontrar recursos dirigidos a estudiantes de otros programas académicos</p>
+                <p class="answerFAQ">El repositorio va dirigido principalmente a usuarios con énfasis en contenidos de Ingeniería de Sistemas. Sin embargo, existe la posibilidad de poder publicar y encontrar recursos enfocados en otros programas académicos.</p>
             </div>
         </div>
         <!-- Pregunta #3 -->
         <div class="parentTabFAQ">
             <input type="radio" name="tab" id="tab-3" class="inputFAQ">
             <label for="tab-3" class="labelFAQ">
-                <span class="questionFAQ">¿Qué tipo de archivos puedo encontrar?</span>
+                <span class="questionFAQ">¿Qué tipo de archivos se pueden encontrar en el repositorio institucional?</span>
                 <div class="iconContainer">
                     <div class="iconFAQ">
                         <i class="uil uil-angle-down"></i>
@@ -50,14 +49,15 @@
                 </div>
             </label>
             <div class="contentFAQ">
-                <p class="answerFAQ">En este repositorio institucional se pueden encontrar tipos de archivos como...</p>
+                <p class="answerFAQ">En este repositorio institucional se pueden encontrar libros, vídeos, imágenes, audios, diapositivas, enlaces, entre otros. 
+                </p>
             </div>
         </div>
         <!-- Pregunta #4 -->
         <div class="parentTabFAQ">
             <input type="radio" name="tab" id="tab-4" class="inputFAQ">
             <label for="tab-4" class="labelFAQ">                
-                <span class="questionFAQ">¿Quiénes pueden hacer uso del repositorio institucional?</span>
+                <span class="questionFAQ">¿Quiénes pueden tener acceso a los contenidos publicados en el repositorio institucional?</span>
                 <div class="iconContainer">
                     <div class="iconFAQ">
                         <i class="uil uil-angle-down"></i>
@@ -65,14 +65,14 @@
                 </div>
             </label>
             <div class="contentFAQ">
-                <p class="answerFAQ">Open Acces :)</p>
+                <p class="answerFAQ">Los contenidos que se encuentren disponibles en el repositorio pueden ser accedidos por todos los usuarios que deseen, tengan o no tengan una cuenta asociada. Esto es así, ya que se tiene la premisa del acceso abierto libre para todos, de forma que se tenga una visibilidad total y una completa difusión de los documentos publicados.</p>
             </div>
         </div>
         <!-- Pregunta #5 -->
         <div class="parentTabFAQ">
             <input type="radio" name="tab" id="tab-5" class="inputFAQ">
             <label for="tab-5" class="labelFAQ">
-                <span class="questionFAQ">¿Cómo puedo publicar un recurso?</span>
+                <span class="questionFAQ">¿Cómo puedo publicar un recurso académico?</span>
                 <div class="iconContainer">
                     <div class="iconFAQ">
                         <i class="uil uil-angle-down"></i>
@@ -80,14 +80,14 @@
                 </div>
             </label>
             <div class="contentFAQ">
-                <p class="answerFAQ">Para poder publicar un recurso, se debe poder en contacto con algún docente de la Institución de Educación Superior</p>
+                <p class="answerFAQ">Para poder publicar un recurso académico, se debe poder en contacto con algún docente de la Institución de Educación Superior, o en su defecto, debe enviar un correo electrónico a correo@admin.com.</p>
             </div>
         </div>
         <!-- Pregunta #6 -->
         <div class="parentTabFAQ">
             <input type="radio" name="tab" id="tab-6" class="inputFAQ">
             <label for="tab-6" class="labelFAQ">
-                <span class="questionFAQ">¿Cómo me puedo poner en contacto?</span>
+                <span class="questionFAQ">¿Cómo puedo aportar al repositorio institucional?</span>
                 <div class="iconContainer">
                     <div class="iconFAQ">
                         <i class="uil uil-angle-down"></i>
@@ -95,7 +95,7 @@
                 </div>
             </label>
             <div class="contentFAQ">
-                <p class="answerFAQ">Con admin</p>
+                <p class="answerFAQ">Con el ánimo de poder contar con un repositorio con contenidos de valor para la comunidad, se le permite al usuario aportar por medio de calificaciones a los recursos, marcando si estos han sido útiles o no. De esta manera, en el repositorio se puede realizar depuración, dejando así únicamente los contenidos que mayor aporte han tenido para los usuarios.</p>
             </div>
         </div>
     </section>
