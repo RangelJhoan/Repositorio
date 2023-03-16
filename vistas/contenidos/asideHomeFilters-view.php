7ebbac9a114ec040e1b59456b6bf323e0add4953
ValenciaJCamilo <camilovalncias@gmail.com>
2023-03-16 17:11:19 -0500

    Aside: Tiene archivo
    1. Se ajusta tamaño hexágonos en home
    2. Se añade tabla "Tiene archivo" con opciones "si" o "no" para filtrar

--- vistas/contenidos/asideHomeFilters-view.php
+++ vistas/contenidos/asideHomeFilters-view.php
@@ -8,4 +8,11 @@
                 <li class="liAsideFilters"><a class="aAsideFilters" href="<?php echo SERVER_URL ?>recursosBusqueda/Curso/">Curso <span class="asideFilterCounter">30</span></a></li>
             </ul>
         </nav>
+    <h3 class="asideFiltersTitle hasFile">Tiene archivo</h3>
+        <nav class = "navAsideFilters">
+            <ul class = "ulAsideFilters">
+                <li class="liAsideFilters"><a class="aAsideFilters" href="<?php echo SERVER_URL ?>recursosBusqueda/Autor/">Sí <span class="asideFilterCounter">5</span></a></li>
+                <li class="liAsideFilters"><a class="aAsideFilters" href="<?php echo SERVER_URL ?>recursosBusqueda/Titulo/">No <span class="asideFilterCounter">5</span></a></li>
+            </ul>
+        </nav>
 </aside>
