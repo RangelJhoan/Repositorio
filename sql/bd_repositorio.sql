9544c3b741217944f0f7b9a104a9263fd7521fab
RangelJhoan <jhoanrangels2@gmail.com>
2023-04-13 03:24:08 -0500

    ACTUALIZACION - Se actualiza SQL de la base de datos

--- sql/bd_repositorio.sql
+++ sql/bd_repositorio.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 4.9.5
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 11-02-2023 a las 16:51:00
--- Versión del servidor: 10.4.22-MariaDB
--- Versión de PHP: 8.1.2
+-- Servidor: localhost:3306
+-- Tiempo de generación: 13-04-2023 a las 08:22:59
+-- Versión del servidor: 10.5.16-MariaDB
+-- Versión de PHP: 7.3.32
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,8 +19,24 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `bd_repositorio`
+-- Base de datos: `id19814812_db_repositorio`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `archivo`
+--
+
+CREATE TABLE `archivo` (
+  `id` int(11) NOT NULL,
+  `ruta` varchar(250) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `tamano` bigint(20) NOT NULL,
+  `nombre` varchar(150) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `estado` tinyint(4) NOT NULL,
+  `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `id_recurso` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
 -- --------------------------------------------------------
 
@@ -29,9 +46,23 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `autor` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(30) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `nombre` varchar(30) COLLATE utf8mb4_spanish_ci NOT NULL,
   `apellido` varchar(30) COLLATE utf8mb4_spanish_ci NOT NULL,
-  `estado` tinyint(4) NOT NULL
+  `estado` tinyint(4) NOT NULL,
+  `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `id_docente` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `autor_recurso`
+--
+
+CREATE TABLE `autor_recurso` (
+  `id` int(11) NOT NULL,
+  `id_autor` int(11) NOT NULL,
+  `id_recurso` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
 -- --------------------------------------------------------
@@ -44,7 +75,8 @@ CREATE TABLE `curso` (
   `id` int(11) NOT NULL,
   `nombre` varchar(100) COLLATE utf8mb4_spanish_ci NOT NULL,
   `descripcion` varchar(255) COLLATE utf8mb4_spanish_ci NOT NULL,
-  `estado` tinyint(4) NOT NULL
+  `estado` tinyint(4) NOT NULL,
+  `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
 -- --------------------------------------------------------
@@ -62,6 +94,18 @@ CREATE TABLE `curso_programa` (
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `curso_recurso`
+--
+
+CREATE TABLE `curso_recurso` (
+  `id` int(11) NOT NULL,
+  `id_curso` int(11) NOT NULL,
+  `id_recurso` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `docente_curso`
 --
 
@@ -69,6 +113,32 @@ CREATE TABLE `docente_curso` (
   `id` int(11) NOT NULL,
   `id_docente` int(11) NOT NULL,
   `id_curso` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `etiqueta`
+--
+
+CREATE TABLE `etiqueta` (
+  `id` int(11) NOT NULL,
+  `descripcion` varchar(60) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `estado` tinyint(4) NOT NULL,
+  `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `id_docente` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `etiqueta_recurso`
+--
+
+CREATE TABLE `etiqueta_recurso` (
+  `id` int(11) NOT NULL,
+  `id_etiqueta` int(11) NOT NULL,
+  `id_recurso` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
 -- --------------------------------------------------------
@@ -83,9 +153,17 @@ CREATE TABLE `persona` (
   `nombre` varchar(30) COLLATE utf8mb4_spanish_ci NOT NULL,
   `apellido` varchar(30) COLLATE utf8mb4_spanish_ci NOT NULL,
   `estado` tinyint(4) NOT NULL,
+  `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `id_tipo_documento` int(11) NOT NULL,
   `id_usuario` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `persona`
+--
+
+INSERT INTO `persona` (`id`, `documento`, `nombre`, `apellido`, `estado`, `fecha_creacion`, `id_tipo_documento`, `id_usuario`) VALUES
+(1, '999999999', 'Super', 'Admin', 1, '2023-03-29 03:46:36', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -97,7 +175,54 @@ CREATE TABLE `programa` (
   `id` int(11) NOT NULL,
   `nombre` varchar(100) COLLATE utf8mb4_spanish_ci NOT NULL,
   `descripcion` varchar(255) COLLATE utf8mb4_spanish_ci NOT NULL,
-  `estado` tinyint(4) NOT NULL
+  `estado` tinyint(4) NOT NULL,
+  `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `puntuacion_recurso`
+--
+
+CREATE TABLE `puntuacion_recurso` (
+  `id` int(11) NOT NULL,
+  `tipo_voto` tinyint(4) NOT NULL,
+  `id_recurso` int(11) NOT NULL,
+  `id_estudiante` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `recurso`
+--
+
+CREATE TABLE `recurso` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(250) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `fecha_publicacion_profesor` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `fecha_publicacion_recurso` varchar(4) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `resumen` varchar(250) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `puntos_positivos` int(11) NOT NULL,
+  `puntos_negativos` int(11) NOT NULL,
+  `estado` tinytext COLLATE utf8mb4_spanish_ci NOT NULL,
+  `enlace` varchar(300) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `isbn` varchar(20) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `editorial` varchar(80) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `id_docente` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `recurso_favorito`
+--
+
+CREATE TABLE `recurso_favorito` (
+  `id` int(11) NOT NULL,
+  `id_persona` int(11) NOT NULL,
+  `id_recurso` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
 -- --------------------------------------------------------
@@ -111,6 +236,15 @@ CREATE TABLE `tipo_documento` (
   `descripcion` varchar(30) COLLATE utf8mb4_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
+--
+-- Volcado de datos para la tabla `tipo_documento`
+--
+
+INSERT INTO `tipo_documento` (`id`, `descripcion`) VALUES
+(1, 'Cédula Ciudadanía'),
+(3, 'Cédula Extranjería'),
+(2, 'Tarjeta Identidad');
+
 -- --------------------------------------------------------
 
 --
@@ -121,6 +255,15 @@ CREATE TABLE `tipo_usuario` (
   `id` int(11) NOT NULL,
   `descripcion` varchar(20) COLLATE utf8mb4_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `tipo_usuario`
+--
+
+INSERT INTO `tipo_usuario` (`id`, `descripcion`) VALUES
+(1, 'Administrador'),
+(2, 'Docente'),
+(3, 'Estudiante');
 
 -- --------------------------------------------------------
 
@@ -133,18 +276,42 @@ CREATE TABLE `usuario` (
   `correo` varchar(60) COLLATE utf8mb4_spanish_ci NOT NULL,
   `clave` varchar(80) COLLATE utf8mb4_spanish_ci NOT NULL,
   `estado` tinyint(4) NOT NULL,
+  `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `id_tipo_usuario` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `correo`, `clave`, `estado`, `fecha_creacion`, `id_tipo_usuario`) VALUES
+(1, 'sa.repositorioinstitucional@gmail.com', 'UU5FTTluWTBsZWVuY0hBaHBYN052dz09', 1, '2023-04-13 08:22:30', 1);
 
 --
 -- Índices para tablas volcadas
 --
 
 --
+-- Indices de la tabla `archivo`
+--
+ALTER TABLE `archivo`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_archivo_recurso` (`id_recurso`);
+
+--
 -- Indices de la tabla `autor`
 --
 ALTER TABLE `autor`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_autor_docente` (`id_docente`);
+
+--
+-- Indices de la tabla `autor_recurso`
+--
+ALTER TABLE `autor_recurso`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_autor_recurso_autor` (`id_autor`),
+  ADD KEY `fk_autor_recurso_recurso` (`id_recurso`);
 
 --
 -- Indices de la tabla `curso`
@@ -162,12 +329,36 @@ ALTER TABLE `curso_programa`
   ADD KEY `fk_curso_programa_programa` (`id_programa`);
 
 --
+-- Indices de la tabla `curso_recurso`
+--
+ALTER TABLE `curso_recurso`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_curso_recurso_curso` (`id_curso`),
+  ADD KEY `fk_curso_recurso_recurso` (`id_recurso`);
+
+--
 -- Indices de la tabla `docente_curso`
 --
 ALTER TABLE `docente_curso`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_docente_curso_docente` (`id_docente`),
   ADD KEY `fk_docente_curso_curso` (`id_curso`);
+
+--
+-- Indices de la tabla `etiqueta`
+--
+ALTER TABLE `etiqueta`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `eti_uniq_descripcion` (`descripcion`),
+  ADD KEY `fk_etiqueta_docente` (`id_docente`);
+
+--
+-- Indices de la tabla `etiqueta_recurso`
+--
+ALTER TABLE `etiqueta_recurso`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_etiqueta_recurso_etiqueta` (`id_etiqueta`),
+  ADD KEY `fk_etiqueta_recurso_recurso` (`id_recurso`);
 
 --
 -- Indices de la tabla `persona`
@@ -184,6 +375,29 @@ ALTER TABLE `persona`
 ALTER TABLE `programa`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `pro_uniq_nombre` (`nombre`);
+
+--
+-- Indices de la tabla `puntuacion_recurso`
+--
+ALTER TABLE `puntuacion_recurso`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_puntuacion_recurso_recurso` (`id_recurso`),
+  ADD KEY `fk_puntuacion_recurso_estudiante` (`id_estudiante`);
+
+--
+-- Indices de la tabla `recurso`
+--
+ALTER TABLE `recurso`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_recurso_docente` (`id_docente`);
+
+--
+-- Indices de la tabla `recurso_favorito`
+--
+ALTER TABLE `recurso_favorito`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_recurso_favorito_persona` (`id_persona`),
+  ADD KEY `fk_recurso_favorito_recurso` (`id_recurso`);
 
 --
 -- Indices de la tabla `tipo_documento`
@@ -212,9 +426,21 @@ ALTER TABLE `usuario`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `archivo`
+--
+ALTER TABLE `archivo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `autor`
 --
 ALTER TABLE `autor`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `autor_recurso`
+--
+ALTER TABLE `autor_recurso`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
@@ -230,16 +456,34 @@ ALTER TABLE `curso_programa`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT de la tabla `curso_recurso`
+--
+ALTER TABLE `curso_recurso`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `docente_curso`
 --
 ALTER TABLE `docente_curso`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT de la tabla `etiqueta`
+--
+ALTER TABLE `etiqueta`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `etiqueta_recurso`
+--
+ALTER TABLE `etiqueta_recurso`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `persona`
 --
 ALTER TABLE `persona`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `programa`
@@ -248,26 +492,63 @@ ALTER TABLE `programa`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT de la tabla `puntuacion_recurso`
+--
+ALTER TABLE `puntuacion_recurso`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `recurso`
+--
+ALTER TABLE `recurso`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `recurso_favorito`
+--
+ALTER TABLE `recurso_favorito`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `tipo_documento`
 --
 ALTER TABLE `tipo_documento`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `tipo_usuario`
 --
 ALTER TABLE `tipo_usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `archivo`
+--
+ALTER TABLE `archivo`
+  ADD CONSTRAINT `archivo_ibfk_2` FOREIGN KEY (`id_recurso`) REFERENCES `recurso` (`id`);
+
+--
+-- Filtros para la tabla `autor`
+--
+ALTER TABLE `autor`
+  ADD CONSTRAINT `autor_ibfk_1` FOREIGN KEY (`id_docente`) REFERENCES `persona` (`id`);
+
+--
+-- Filtros para la tabla `autor_recurso`
+--
+ALTER TABLE `autor_recurso`
+  ADD CONSTRAINT `autor_recurso_ibfk_1` FOREIGN KEY (`id_autor`) REFERENCES `autor` (`id`),
+  ADD CONSTRAINT `autor_recurso_ibfk_2` FOREIGN KEY (`id_recurso`) REFERENCES `recurso` (`id`);
 
 --
 -- Filtros para la tabla `curso_programa`
@@ -277,6 +558,13 @@ ALTER TABLE `curso_programa`
   ADD CONSTRAINT `curso_programa_ibfk_2` FOREIGN KEY (`id_curso`) REFERENCES `curso` (`id`);
 
 --
+-- Filtros para la tabla `curso_recurso`
+--
+ALTER TABLE `curso_recurso`
+  ADD CONSTRAINT `curso_recurso_ibfk_1` FOREIGN KEY (`id_curso`) REFERENCES `curso` (`id`),
+  ADD CONSTRAINT `curso_recurso_ibfk_2` FOREIGN KEY (`id_recurso`) REFERENCES `recurso` (`id`);
+
+--
 -- Filtros para la tabla `docente_curso`
 --
 ALTER TABLE `docente_curso`
@@ -284,11 +572,44 @@ ALTER TABLE `docente_curso`
   ADD CONSTRAINT `docente_curso_ibfk_2` FOREIGN KEY (`id_curso`) REFERENCES `curso` (`id`);
 
 --
+-- Filtros para la tabla `etiqueta`
+--
+ALTER TABLE `etiqueta`
+  ADD CONSTRAINT `etiqueta_ibfk_1` FOREIGN KEY (`id_docente`) REFERENCES `persona` (`id`);
+
+--
+-- Filtros para la tabla `etiqueta_recurso`
+--
+ALTER TABLE `etiqueta_recurso`
+  ADD CONSTRAINT `etiqueta_recurso_ibfk_1` FOREIGN KEY (`id_etiqueta`) REFERENCES `etiqueta` (`id`),
+  ADD CONSTRAINT `etiqueta_recurso_ibfk_2` FOREIGN KEY (`id_recurso`) REFERENCES `recurso` (`id`);
+
+--
 -- Filtros para la tabla `persona`
 --
 ALTER TABLE `persona`
   ADD CONSTRAINT `persona_ibfk_1` FOREIGN KEY (`id_tipo_documento`) REFERENCES `tipo_documento` (`id`),
   ADD CONSTRAINT `persona_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`);
+
+--
+-- Filtros para la tabla `puntuacion_recurso`
+--
+ALTER TABLE `puntuacion_recurso`
+  ADD CONSTRAINT `puntuacion_recurso_ibfk_1` FOREIGN KEY (`id_estudiante`) REFERENCES `persona` (`id`),
+  ADD CONSTRAINT `puntuacion_recurso_ibfk_2` FOREIGN KEY (`id_recurso`) REFERENCES `recurso` (`id`);
+
+--
+-- Filtros para la tabla `recurso`
+--
+ALTER TABLE `recurso`
+  ADD CONSTRAINT `recurso_ibfk_1` FOREIGN KEY (`id_docente`) REFERENCES `persona` (`id`);
+
+--
+-- Filtros para la tabla `recurso_favorito`
+--
+ALTER TABLE `recurso_favorito`
+  ADD CONSTRAINT `recurso_favorito_ibfk_1` FOREIGN KEY (`id_persona`) REFERENCES `persona` (`id`),
+  ADD CONSTRAINT `recurso_favorito_ibfk_2` FOREIGN KEY (`id_recurso`) REFERENCES `recurso` (`id`);
 
 --
 -- Filtros para la tabla `usuario`
