25b29eb726b45a864633124b3180da53e6a956de
RangelJhoan <jhoanrangels2@gmail.com>
2023-05-20 20:46:26 -0500

    EXTRA - Base de datos vacía y README de base de datos
    Se agrega el sql de la base de datos vacía la cual será entregada como base de datos real
    Se agrega README con la explicación de la diferencia entre las dos bases de datos y los credenciales de prueba

---
+++ sql/README.md
@@ -0,0 +1,22 @@
+#BASES DE DATOS DEL PROTOTIPO FUNCIONAL DEL BANCO DE RECURSOS DIGITALES AUDIOVISUALES PARA LOS CURSOS DEL PROGRAMA DE INGENIERÍA DE SISTEMAS.
+------------
+### Base de datos de prueba
+La base de datos de prueba (bd_repositorio.sql) **cuenta con datos** que permiten **ejecutar y validadar las funcionalidades** que se encuentran disponibles en el repositorio institucional. Cuenta con **datos ficticios** (usuarios, programas, cursos, recursos, palabras clave, autores) que dan posibilidad de hacer uso del proyecto de inmediato y sin complicación alguna.
+Para poder acceder a las funcionalidades disponibles de cada uno de los usuarios, a continuación se presentan las **credenciales de acceso (de prueba)** para verificar el funcionamiento.
+
+| ACTOR  | CORREO  | CONTRASEÑA |
+| :------------ |:---------------:| -----:|
+| Superadministrador     | sa.repositorioinstitucional@gmail.com | Repo2022* |
+| Administrador      | msmith@gmail.com        |   Repo2022*|
+| Docente | juanse1010@gmail.com        |    Repo2022* |
+| Estudiante | camilovalncias@gmail.com       |    Repo2022* |
+
+
+### Base de datos vacía
+Esta base de datos (bd_repositorio_vacio.sql) cuenta sólo con la información necesaria en el repositorio tales como tipo de documento (CC, TI, TE) y tipo de usuario (Administrador, Docente y Estudiante). Al usar esta base de datos, **no se encontrará información sobre  usuarios, programas, cursos, recursos, palabras clave, o autores**, motivo por el cual  se debe iniciar con el superadministrador para poder dar inicio a la gestión completa del prototipo funcional. 
+
+| ACTOR  | CORREO  | CONTRASEÑA |
+| :------------ |:---------------:| -----:|
+| Superadministrador     | sa.repositorioinstitucional@gmail.com | Repo2022* |
+
+
---
+++ sql/bd_repositorio_vacio.sql
@@ -0,0 +1,621 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost:3306
+-- Tiempo de generación: 21-05-2023 a las 00:43:15
+-- Versión del servidor: 10.5.16-MariaDB
+-- Versión de PHP: 7.3.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `id19814812_db_repositorio`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `archivo`
+--
+
+CREATE TABLE `archivo` (
+  `id` int(11) NOT NULL,
+  `ruta` varchar(250) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `tamano` bigint(20) NOT NULL,
+  `nombre` varchar(150) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `estado` tinyint(4) NOT NULL,
+  `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `id_recurso` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `autor`
+--
+
+CREATE TABLE `autor` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(30) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `apellido` varchar(30) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `estado` tinyint(4) NOT NULL,
+  `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `id_docente` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `autor_recurso`
+--
+
+CREATE TABLE `autor_recurso` (
+  `id` int(11) NOT NULL,
+  `id_autor` int(11) NOT NULL,
+  `id_recurso` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `curso`
+--
+
+CREATE TABLE `curso` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `descripcion` varchar(255) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `estado` tinyint(4) NOT NULL,
+  `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `curso_programa`
+--
+
+CREATE TABLE `curso_programa` (
+  `id` int(11) NOT NULL,
+  `id_curso` int(11) NOT NULL,
+  `id_programa` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `curso_recurso`
+--
+
+CREATE TABLE `curso_recurso` (
+  `id` int(11) NOT NULL,
+  `id_curso` int(11) NOT NULL,
+  `id_recurso` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `docente_curso`
+--
+
+CREATE TABLE `docente_curso` (
+  `id` int(11) NOT NULL,
+  `id_docente` int(11) NOT NULL,
+  `id_curso` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `etiqueta`
+--
+
+CREATE TABLE `etiqueta` (
+  `id` int(11) NOT NULL,
+  `descripcion` varchar(60) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `estado` tinyint(4) NOT NULL,
+  `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `id_docente` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `etiqueta_recurso`
+--
+
+CREATE TABLE `etiqueta_recurso` (
+  `id` int(11) NOT NULL,
+  `id_etiqueta` int(11) NOT NULL,
+  `id_recurso` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `persona`
+--
+
+CREATE TABLE `persona` (
+  `id` int(11) NOT NULL,
+  `documento` varchar(15) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `nombre` varchar(30) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `apellido` varchar(30) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `estado` tinyint(4) NOT NULL,
+  `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `id_tipo_documento` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `persona`
+--
+
+INSERT INTO `persona` (`id`, `documento`, `nombre`, `apellido`, `estado`, `fecha_creacion`, `id_tipo_documento`, `id_usuario`) VALUES
+(1, '999999999', 'Super', 'Admin', 1, '2023-05-21 00:42:18', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `programa`
+--
+
+CREATE TABLE `programa` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `descripcion` varchar(255) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `estado` tinyint(4) NOT NULL,
+  `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `puntuacion_recurso`
+--
+
+CREATE TABLE `puntuacion_recurso` (
+  `id` int(11) NOT NULL,
+  `tipo_voto` tinyint(4) NOT NULL,
+  `id_recurso` int(11) NOT NULL,
+  `id_estudiante` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `recurso`
+--
+
+CREATE TABLE `recurso` (
+  `id` int(11) NOT NULL,
+  `internal_id` varchar(24) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `titulo` varchar(250) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `fecha_publicacion_profesor` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `fecha_publicacion_recurso` varchar(4) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `resumen` varchar(1500) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `puntos_positivos` int(11) NOT NULL,
+  `puntos_negativos` int(11) NOT NULL,
+  `estado` tinytext COLLATE utf8mb4_spanish_ci NOT NULL,
+  `enlace` varchar(300) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `isbn` varchar(20) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `editorial` varchar(80) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `id_docente` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `recurso_favorito`
+--
+
+CREATE TABLE `recurso_favorito` (
+  `id` int(11) NOT NULL,
+  `id_persona` int(11) NOT NULL,
+  `id_recurso` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_documento`
+--
+
+CREATE TABLE `tipo_documento` (
+  `id` int(11) NOT NULL,
+  `descripcion` varchar(30) COLLATE utf8mb4_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `tipo_documento`
+--
+
+INSERT INTO `tipo_documento` (`id`, `descripcion`) VALUES
+(1, 'Cédula Ciudadanía'),
+(3, 'Cédula Extranjería'),
+(2, 'Tarjeta Identidad');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_usuario`
+--
+
+CREATE TABLE `tipo_usuario` (
+  `id` int(11) NOT NULL,
+  `descripcion` varchar(20) COLLATE utf8mb4_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `tipo_usuario`
+--
+
+INSERT INTO `tipo_usuario` (`id`, `descripcion`) VALUES
+(1, 'Administrador'),
+(2, 'Docente'),
+(3, 'Estudiante');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
+  `correo` varchar(60) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `clave` varchar(80) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `estado` tinyint(4) NOT NULL,
+  `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `id_tipo_usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `correo`, `clave`, `estado`, `fecha_creacion`, `id_tipo_usuario`) VALUES
+(1, 'sa.repositorioinstitucional@gmail.com', 'UU5FTTluWTBsZWVuY0hBaHBYN052dz09', 1, '2023-05-21 00:42:01', 1);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `archivo`
+--
+ALTER TABLE `archivo`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_archivo_recurso` (`id_recurso`);
+
+--
+-- Indices de la tabla `autor`
+--
+ALTER TABLE `autor`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_autor_docente` (`id_docente`);
+
+--
+-- Indices de la tabla `autor_recurso`
+--
+ALTER TABLE `autor_recurso`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_autor_recurso_autor` (`id_autor`),
+  ADD KEY `fk_autor_recurso_recurso` (`id_recurso`);
+
+--
+-- Indices de la tabla `curso`
+--
+ALTER TABLE `curso`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `curso_programa`
+--
+ALTER TABLE `curso_programa`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_curso_programa_curso` (`id_curso`),
+  ADD KEY `fk_curso_programa_programa` (`id_programa`);
+
+--
+-- Indices de la tabla `curso_recurso`
+--
+ALTER TABLE `curso_recurso`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_curso_recurso_curso` (`id_curso`),
+  ADD KEY `fk_curso_recurso_recurso` (`id_recurso`);
+
+--
+-- Indices de la tabla `docente_curso`
+--
+ALTER TABLE `docente_curso`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_docente_curso_docente` (`id_docente`),
+  ADD KEY `fk_docente_curso_curso` (`id_curso`);
+
+--
+-- Indices de la tabla `etiqueta`
+--
+ALTER TABLE `etiqueta`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_etiqueta_docente` (`id_docente`);
+
+--
+-- Indices de la tabla `etiqueta_recurso`
+--
+ALTER TABLE `etiqueta_recurso`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_etiqueta_recurso_etiqueta` (`id_etiqueta`),
+  ADD KEY `fk_etiqueta_recurso_recurso` (`id_recurso`);
+
+--
+-- Indices de la tabla `persona`
+--
+ALTER TABLE `persona`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `per_uniq_documento` (`documento`),
+  ADD KEY `fk_persona_tipo_documento` (`id_tipo_documento`),
+  ADD KEY `fk_persona_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `programa`
+--
+ALTER TABLE `programa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `puntuacion_recurso`
+--
+ALTER TABLE `puntuacion_recurso`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_puntuacion_recurso_recurso` (`id_recurso`),
+  ADD KEY `fk_puntuacion_recurso_estudiante` (`id_estudiante`);
+
+--
+-- Indices de la tabla `recurso`
+--
+ALTER TABLE `recurso`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_recurso_docente` (`id_docente`);
+
+--
+-- Indices de la tabla `recurso_favorito`
+--
+ALTER TABLE `recurso_favorito`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_recurso_favorito_persona` (`id_persona`),
+  ADD KEY `fk_recurso_favorito_recurso` (`id_recurso`);
+
+--
+-- Indices de la tabla `tipo_documento`
+--
+ALTER TABLE `tipo_documento`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `tdoc_uniq_descripcion` (`descripcion`);
+
+--
+-- Indices de la tabla `tipo_usuario`
+--
+ALTER TABLE `tipo_usuario`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `tusu_uniq_descripcion` (`descripcion`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `usu_uniq_correo` (`correo`),
+  ADD KEY `fk_usuario_tipo_usuario` (`id_tipo_usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `archivo`
+--
+ALTER TABLE `archivo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `autor`
+--
+ALTER TABLE `autor`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `autor_recurso`
+--
+ALTER TABLE `autor_recurso`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `curso`
+--
+ALTER TABLE `curso`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `curso_programa`
+--
+ALTER TABLE `curso_programa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `curso_recurso`
+--
+ALTER TABLE `curso_recurso`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `docente_curso`
+--
+ALTER TABLE `docente_curso`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `etiqueta`
+--
+ALTER TABLE `etiqueta`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `etiqueta_recurso`
+--
+ALTER TABLE `etiqueta_recurso`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `persona`
+--
+ALTER TABLE `persona`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `programa`
+--
+ALTER TABLE `programa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `puntuacion_recurso`
+--
+ALTER TABLE `puntuacion_recurso`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `recurso`
+--
+ALTER TABLE `recurso`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `recurso_favorito`
+--
+ALTER TABLE `recurso_favorito`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tipo_documento`
+--
+ALTER TABLE `tipo_documento`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `tipo_usuario`
+--
+ALTER TABLE `tipo_usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `archivo`
+--
+ALTER TABLE `archivo`
+  ADD CONSTRAINT `archivo_ibfk_2` FOREIGN KEY (`id_recurso`) REFERENCES `recurso` (`id`);
+
+--
+-- Filtros para la tabla `autor`
+--
+ALTER TABLE `autor`
+  ADD CONSTRAINT `autor_ibfk_1` FOREIGN KEY (`id_docente`) REFERENCES `persona` (`id`);
+
+--
+-- Filtros para la tabla `autor_recurso`
+--
+ALTER TABLE `autor_recurso`
+  ADD CONSTRAINT `autor_recurso_ibfk_1` FOREIGN KEY (`id_autor`) REFERENCES `autor` (`id`),
+  ADD CONSTRAINT `autor_recurso_ibfk_2` FOREIGN KEY (`id_recurso`) REFERENCES `recurso` (`id`);
+
+--
+-- Filtros para la tabla `curso_programa`
+--
+ALTER TABLE `curso_programa`
+  ADD CONSTRAINT `curso_programa_ibfk_1` FOREIGN KEY (`id_programa`) REFERENCES `programa` (`id`),
+  ADD CONSTRAINT `curso_programa_ibfk_2` FOREIGN KEY (`id_curso`) REFERENCES `curso` (`id`);
+
+--
+-- Filtros para la tabla `curso_recurso`
+--
+ALTER TABLE `curso_recurso`
+  ADD CONSTRAINT `curso_recurso_ibfk_1` FOREIGN KEY (`id_curso`) REFERENCES `curso` (`id`),
+  ADD CONSTRAINT `curso_recurso_ibfk_2` FOREIGN KEY (`id_recurso`) REFERENCES `recurso` (`id`);
+
+--
+-- Filtros para la tabla `docente_curso`
+--
+ALTER TABLE `docente_curso`
+  ADD CONSTRAINT `docente_curso_ibfk_1` FOREIGN KEY (`id_docente`) REFERENCES `persona` (`id`),
+  ADD CONSTRAINT `docente_curso_ibfk_2` FOREIGN KEY (`id_curso`) REFERENCES `curso` (`id`);
+
+--
+-- Filtros para la tabla `etiqueta`
+--
+ALTER TABLE `etiqueta`
+  ADD CONSTRAINT `etiqueta_ibfk_1` FOREIGN KEY (`id_docente`) REFERENCES `persona` (`id`);
+
+--
+-- Filtros para la tabla `etiqueta_recurso`
+--
+ALTER TABLE `etiqueta_recurso`
+  ADD CONSTRAINT `etiqueta_recurso_ibfk_1` FOREIGN KEY (`id_etiqueta`) REFERENCES `etiqueta` (`id`),
+  ADD CONSTRAINT `etiqueta_recurso_ibfk_2` FOREIGN KEY (`id_recurso`) REFERENCES `recurso` (`id`);
+
+--
+-- Filtros para la tabla `persona`
+--
+ALTER TABLE `persona`
+  ADD CONSTRAINT `persona_ibfk_1` FOREIGN KEY (`id_tipo_documento`) REFERENCES `tipo_documento` (`id`),
+  ADD CONSTRAINT `persona_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`);
+
+--
+-- Filtros para la tabla `puntuacion_recurso`
+--
+ALTER TABLE `puntuacion_recurso`
+  ADD CONSTRAINT `puntuacion_recurso_ibfk_1` FOREIGN KEY (`id_estudiante`) REFERENCES `persona` (`id`),
+  ADD CONSTRAINT `puntuacion_recurso_ibfk_2` FOREIGN KEY (`id_recurso`) REFERENCES `recurso` (`id`);
+
+--
+-- Filtros para la tabla `recurso`
+--
+ALTER TABLE `recurso`
+  ADD CONSTRAINT `recurso_ibfk_1` FOREIGN KEY (`id_docente`) REFERENCES `persona` (`id`);
+
+--
+-- Filtros para la tabla `recurso_favorito`
+--
+ALTER TABLE `recurso_favorito`
+  ADD CONSTRAINT `recurso_favorito_ibfk_1` FOREIGN KEY (`id_persona`) REFERENCES `persona` (`id`),
+  ADD CONSTRAINT `recurso_favorito_ibfk_2` FOREIGN KEY (`id_recurso`) REFERENCES `recurso` (`id`);
+
+--
+-- Filtros para la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`id_tipo_usuario`) REFERENCES `tipo_usuario` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
